9dc0828b8da6de7e1ab60e6fa23be3c45c58e8b3
devinspencer <devinspencer@169-231-132-203.wireless.ucsb.edu>
2016-10-13 14:49:58 -0700

    changing read.csv to read_csv

--- ui.R
+++ ui.R
@@ -10,12 +10,12 @@ library(rsconnect) #Used to deploy shiny app to shinyapp.io for inclusion in web
 
 ######To deploy to the shiny.io server you must use relative path names and your data must be in a data folder within the folder that contains the ui.R file and the server.R file. Only files in the folder and subfolders of your Shiny app are made available to shinyapps.io! ######### 
 
-# DeepFish <- read.csv("data/deep_water_fish_diversity/fish_diversity.csv",header=TRUE)
-# Mobile <- read.csv("data/kelp_forest/mobileinvertbrate_diversity_web.csv",header=TRUE)
-# FishDensity<- read.csv("data/kelp_forest/fish_density_web.csv",header=TRUE)
-# Kelpbio<- read.csv("data/kelp_forest/kelp_biomass_web.csv", header=TRUE)
-
-site <- as.character(unique(DeepFish$site)) ##Convert site names to list - if these differ between datasets, you will have to change
+# DeepFish <- read_csv("data/deep_water_fish_diversity/fish_diversity.csv")
+# # Mobile <- read.csv("data/kelp_forest/mobileinvertbrate_diversity_web.csv",header=TRUE)
+# # FishDensity<- read.csv("data/kelp_forest/fish_density_web.csv",header=TRUE)
+# # Kelpbio<- read.csv("data/kelp_forest/kelp_biomass_web.csv", header=TRUE)
+# 
+# site <- as.character(unique(DeepFish$site)) ##Convert site names to list - if these differ between datasets, you will have to change
 
 shinyUI(
   fluidPage(
